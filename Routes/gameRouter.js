0e2324963dde268f37b7a44223bf654e65e7be75
fathy soliman <fathi123soliman@gmail.com>
2026-04-13 14:54:05 +0300

    finish game setup

--- Routes/gameRouter.js
+++ Routes/gameRouter.js
@@ -2,5 +2,7 @@ const router = require("express").Router()
 const gameController = require("../controllers/gameController")
 
 router.post("/", gameController.CreateGame)
+router.get("/", gameController.GetAll)
+router.delete("/:id", gameController.DeleteGame)
 
 module.exports = router
