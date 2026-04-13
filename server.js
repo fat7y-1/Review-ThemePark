6b376b545a3b3db1830c1f4eca7a4c029be947bb
zainabsalman <zsalman380@gmail.com>
2026-04-13 15:35:12 +0300

    reviewrouteandcontroller

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const logger = require("morgan")
 
 const gameRouter = require("./Routes/gameRouter")
-
+const reviewRouter = require("./Routes/reviewRouter")
 const PORT = process.env.PORT || 3000
 const dns = require("dns")
 dns.setServers(["8.8.8.8", "1.1.1.1"])
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use("/game", gameRouter)
-
+app.use("/review", reviewRouter)
 app.use("/", (req, res) => {
   res.send(`Connected!`)
 })
