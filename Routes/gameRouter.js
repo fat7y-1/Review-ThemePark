f320393f6de1e3a5b60f3760fe73a7f9cd48ec60
zainabsalman <zsalman380@gmail.com>
2026-04-13 15:06:47 +0300

    test

--- Routes/gameRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const gameController = require("../controllers/gameController")
-
-router.post("/", gameController.CreateGame)
-router.get("/", gameController.GetAll)
-router.delete("/:id", gameController.DeleteGame)
-
-module.exports = router
